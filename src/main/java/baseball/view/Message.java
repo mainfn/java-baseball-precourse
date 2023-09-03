3a04eef9809be6c8a1a29520757386993700b77a
mainfn <_@mainfn.dev>
2023-09-03 15:25:38 +0900

    feat: 입출력 메세지

---
+++ src/main/java/baseball/view/Message.java
@@ -0,0 +1,8 @@
+package baseball.view;
+
+public final class Message {
+
+  public static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
+  public static final String GAME_CLEAR = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+}
