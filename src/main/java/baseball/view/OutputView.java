73dcd652153bfe16bb9868c32188a069feeb4e84
mainfn <_@mainfn.dev>
2023-09-03 15:26:58 +0900

    feat: 메세지 출력 기능

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public final class OutputView {
+
+  public void printInputNumbersMessage() {
+    System.out.print(Message.INPUT_NUMBERS);
+  }
+
+  public void printGameClearMessage() {
+    System.out.println(Message.GAME_CLEAR);
+  }
+}
