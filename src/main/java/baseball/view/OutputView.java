3e6ce325aacebf0ccadf5dece78c9b69da5e7cca
mainfn <_@mainfn.dev>
2023-10-02 13:16:26 +0900

    feat: 출력 기능 생성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,10 @@
 package baseball.view;
 
+import baseball.domain.GuessResult;
+
 public final class OutputView {
 
+  public static void printGuessResult(final GuessResult result) {
+    System.out.println(result);
+  }
 }
